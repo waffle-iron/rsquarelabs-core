c315801e24a9e56f80d6b0a34fea79d65b9c6cdb
Ravi RT Merugu <rrmerugu@gmail.com>
2015-12-08 14:49:56 +0530

    setup updated

--- main.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'rrmerugu'
-
-
-def main():
-    print "Hello World!, I'm RSQUARE LABS !"
-
-if __name__ == '__main__':
-    main()
--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ from distutils.core import setup
 readme = open('README.txt').read()
 
 setup(name='rsquarelabs',
-version='0.0.1a',
+version='0.0.1alpha',
 description='This is the library of automation pipeline modules developed at RSQUARE LABS.',
 long_description= readme,
 author='Ravi RT Merugu',
 author_email='rrmerugu@gmail.com',
-url = 'http://github.com/rsquarelabs/rsquarelabs-py',
-py_modules = ['main'],
+url = 'http://github.com/rsquarelabs/rsquarelabs',
+py_modules = ['rsquarelabs'],
 keywords = ['Computational Biology', 'Molecular Modelling', 'Bioinformatics', 'Automation'])
