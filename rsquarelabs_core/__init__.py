2db572c7938c4ea36bd1ae1ea76a72c8708d7020
nitish <nitish.hp.515@gmail.com>
2016-05-23 14:51:22 +0530

    Made the tool works only with python version 2.7

--- rsquarelabs_core/__init__.py
+++ rsquarelabs_core/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'rrmerugu'
 
-import os
+import os, sys
 from .config import RSQ_PROJECTS_HOME, RSQ_HOME, RSQ_DB_PATH, RSQ_PROJECTS_CONFIG
 
 if not os.path.exists(RSQ_PROJECTS_HOME):
@@ -12,4 +12,6 @@ if not os.path.exists(RSQ_HOME):
 if not os.path.exists(RSQ_PROJECTS_CONFIG): # not very much needed
     os.mkdir(RSQ_PROJECTS_CONFIG, 0755)
 
-
+if not sys.version_info[:2] == (2, 7):
+    print ("Sorry! This tool works only with python version 2.7.")
+    exit()
